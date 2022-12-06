1c79cd317e18a4881e5be24b1f53a4902e99b78e
sd31117 <sd31117@hotmail.com>
2022-12-06 04:51:15 -0600

    init login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (isset($_POST['submit'])) {
+    $_SESSION['user'] = htmlspecialchars($_POST['username']);
+    header('Location: /');
+}
+?>
+
+You must be logged in to do that!
+<form method="POST">
+    <input type="text" name="username" id="username-input" placeholder="Username">
+    <input type="password" name="password" id="password-input" placeholder="Password">
+    <input type="submit" name="submit" id="login-button" value="Submit">
+</form>
