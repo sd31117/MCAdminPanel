232473287a8790d7420d6d68423eed8c51b748e7
sd31117 <sd31117@hotmail.com>
2022-12-08 00:38:03 -0600

    set up index with new routing system

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 include 'includes/session.php';
 
+// echo 'Session vars: <br>';
+// print_r($_SESSION);
+// echo '<br><br>';
+
 $app = new Application();
+
+$app->add('', 'home.php');
+$app->add('login', 'login.php');
+$app->add('logout', 'logout.php');
+
+if (@$app->route($_SERVER['REQUEST_URI']) === false) {
+    $app->throw(404);
+}
