b542599ced4d2e5b7e27de1bc86147d5aeffcfae
sd31117 <sd31117@hotmail.com>
2022-12-08 00:37:07 -0600

    fixed routing for non existant endpoints and added errors

--- classes/Router.php
+++ classes/Router.php
@@ -6,7 +6,30 @@ class Router
 
     public function add($path, $callback)
     {
+        $path = '/' . $path;
         $this->routes += [$path => $callback];
+    }
+
+    public function route($urlPath)
+    {
+        if (($callback = $this->routes[$urlPath]) === null) {
+            return false;
+        }
+
+        include_once 'public/' . $callback;
+    }
+
+    public function throw($errorCode)
+    {
+        switch ($errorCode) {
+            case 404:
+                header('HTTP1.1/ 404 Not Found', true, $errorCode);
+                break;
+
+            default:
+                header('HTTP1.1/500 Server Error', true, 500);
+                break;
+        }
     }
 
     public function showRoutes()
