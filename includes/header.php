2c3e616f44ec545f446acf5cba1850a499edef8a
sd31117 <sd31117@hotmail.com>
2022-12-15 02:34:53 -0600

    created header file, DRY

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div id='logo-container'>
+        <img src="assets/img/mcadminlogo.png" alt="logo">
+        <h1>MCAdminPanel</h1>
+    </div>
+    <div id="burger"><a href="/logout">Logout</a></div>
+</header>
